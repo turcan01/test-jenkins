bbb3861c091e719b9be280dfbc10d22a0aa8b329
turcan01 <turcan.guvener@gmail.com>
2023-03-03 20:57:25 -0600

    fgdgdf

--- modules/aws_ec2/outputs.tf
+++ modules/aws_ec2/outputs.tf
@@ -1,3 +1,6 @@
 output "id" {
   value = aws_instance.instance.id
 }
+output "ipv4" {
+  value = aws_instance.instance.public_ip
+}
--- modules/aws_rds/outputs.tf
+++ modules/aws_rds/outputs.tf
@@ -1,3 +1,6 @@
 output "id" {
   value = aws_db_instance.db.id
 }
+output "endpoint" {
+  value = aws_db_instance.db.endpoint
+}
