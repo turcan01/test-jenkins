6c606fd5ba8c37b17d948ad336f44fda75bf7a29
turcan01 <turcan.guvener@gmail.com>
2023-03-03 20:47:19 -0600

    add8

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,8 @@
-output "instances" {
-    value= module.instances.*.public_ip
+output "instance1" {
+    value= module.aws_ec2.ec2_instance1.public_ip
+}
+output "instance2" {
+    value= module.aws_ec2.ec2_instance2.public_ip
 }
 output "rds_endpoint" {
     value = module.rds_endpoint
